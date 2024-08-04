c08f98071cd4158f2a7b2c0513739183be7975e6
Ash Coelho <ashlakarina02@gmail.com>
2024-08-04 19:07:41 -0300

    ADICIONAR

--- views/SQL.sql
+++ views/SQL.sql
@@ -22,3 +22,18 @@ INSERT INTO usuarios (nome, email, senha, perfil) VALUES
 -- Inserção do administrador inicial
 INSERT INTO usuarios (nome, email, senha, perfil) VALUES 
 ('Administrador', 'admin@exemplo.com', SHA2('senha_admin', 256), 'administrador');
+
+CREATE DATABASE user;
+
+USE user_registration;
+
+CREATE TABLE usuarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    senha VARCHAR(255) NOT NULL,
+    cpf VARCHAR(11) NOT NULL UNIQUE,
+    telefone VARCHAR(12) NOT NULL,
+    nascimento DATE NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
